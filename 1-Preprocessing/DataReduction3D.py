fe7975f1bd1f21adb376b625b8050a454cc2dd54
JoaoSecate <joao.secate@aluno.ifsp.edu.br>
2024-03-14 11:21:57 -0300

    Criado; FrequenceTable e DataReduction3D

---
+++ 1-Preprocessing/DataReduction3D.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+def main():
+    # Faz a leitura do arquivo
+    input_file = '0-Datasets/glass_Normalized.data'
+    names = ['RI','Na','Mg','Al','Si','K','Ca','Ba','Fe','Tipo'] 
+    features = ['RI','Na','Mg','Al','Si','K','Ca','Ba','Fe']
+    target = 'Tipo'
+    df = pd.read_csv(input_file,    # Nome do arquivo com dados
+                     names = names) # Nome das colunas                      
+    ShowInformationDataFrame(df,"Dataframe original")
+
+    # Separating out the features
+    x = df.loc[:, features].values
+
+    # Separating out the target
+    y = df.loc[:,[target]].values
+
+    # Standardizing the features
+    x = StandardScaler().fit_transform(x)
+    normalizedDf = pd.DataFrame(data = x, columns = features)
+    normalizedDf = pd.concat([normalizedDf, df[[target]]], axis = 1)
+    ShowInformationDataFrame(normalizedDf,"Dataframe Normalized")
+
+    # PCA projection
+    pca = PCA(n_components=3)    
+    principalComponents = pca.fit_transform(x)
+    print("Explained variance per component:")
+    print(pca.explained_variance_ratio_.tolist())
+    print("\n\n")
+
+    principalDf = pd.DataFrame(data = principalComponents[:,0:3], 
+                               columns = ['principal component 1', 
+                                          'principal component 2',
+                                          'principal component 3'])
+    finalDf = pd.concat([principalDf, df[[target]]], axis = 1)    
+    ShowInformationDataFrame(finalDf,"Dataframe PCA")
+    
+    VisualizePcaProjection(finalDf, target)
+
+
+def ShowInformationDataFrame(df, message=""):
+    print(message+"\n")
+    print(df.info())
+    print(df.describe())
+    print(df.head(10))
+    print("\n")
+    
+           
+def VisualizePcaProjection(finalDf, targetColumn):
+    fig = plt.figure(figsize = (8,8)) 
+    ax = fig.add_subplot(111, projection = '3d')
+    ax.set_xlabel('Principal Component 1', fontsize = 15)
+    ax.set_ylabel('Principal Component 2', fontsize = 15)
+    ax.set_zlabel('Principal Component 3', fontsize = 15)
+    ax.set_title('3 component PCA', fontsize = 20)
+    targets = [1, 2, 3, 4, 5, 6, 7]
+    colors = ['r', 'g', 'b', 'y', 'c', 'm', 'k']
+    for target, color in zip(targets,colors):
+        indicesToKeep = finalDf[targetColumn] == target
+        ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1'],
+                   finalDf.loc[indicesToKeep, 'principal component 2'],
+                   finalDf.loc[indicesToKeep, 'principal component 3'],
+                   c = color, s = 50)
+    
+    ax.legend(targets)
+    ax.grid()
+    plt.show()
+
+    
+
+
+if __name__ == "__main__":
+    main()
---
+++ 1-Preprocessing/FrequenceTable.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    # Faz a leitura do arquivo
+    input_file = '0-Datasets/glass_Clear.data'
+    names = ['RI','Na','Mg','Al','Si','K','Ca','Ba','Fe']
+    df = pd.read_csv(input_file,    # Nome do arquivo com dados
+                      names = names) # Nome das colunas         
+
+    N = 3;
+    freqtable = pd.cut(df['Na'], bins=N) 
+    freq = freqtable.value_counts().sort_index()
+    print(freq)
+    plt.bar(freq.index.astype(str), freq.values)
+    plt.xlabel('Categorias')
+    plt.ylabel('Frequencias')
+    plt.title('Distribuição de Frequência')
+
+    plt.show()
+
+if __name__ == "__main__":
+    main()
